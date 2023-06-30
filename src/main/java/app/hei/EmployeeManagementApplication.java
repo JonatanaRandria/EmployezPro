de9622f73b3a61c943793d6306b22b4e3a29452e
Ryan Andriamahery <ryan@hei.school>
2023-06-30 08:44:08 +0300

    chore: rename project main classes correctly

--- src/main/java/app/hei/EmployeeManagementApplication.java
+++ src/main/java/app/hei/EmployeeManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiniErpApplication {
+public class EmployeeManagementApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MiniErpApplication.class, args);
+    SpringApplication.run(EmployeeManagementApplication.class, args);
   }
 
 }
